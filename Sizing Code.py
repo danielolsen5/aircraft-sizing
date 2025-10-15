7c9634d920b4df0302067d3d3602f1d7e01278e9
Daniel Olsen <d.olsen517@gmail.com>
2025-10-14 20:53:26 -0600

    WIP: save local changes before rebase

--- Sizing Code.py
+++ Sizing Code.py
@@ -15,10 +15,6 @@ w_w = rho_w * s
 w_s = w / s # wing loading
 g = 9.8 # gravity
 
-if Print:
-    print("Aspect Ratio: ", A)
-    print("Predicted wing weight", w_w)
-
 """ flight properties """ 
 
 v_c = 20 # cruse velocity m/s rho = 1.225 # air density kg/m^3 
@@ -36,24 +32,23 @@ v_s = np.sqrt((2 * w_s) / (rho * c_lm))
 
 # tail sizing
 
-S_w = s # wing area 
+S_w = s # wing area
 b_w = b # wing span
 C_w = (c_r * c_t) / 2 # mean chord
 L_vt = 1.5 # distance from tail quarter chord to wings mean chord leading edge
 L_ht = L_vt 
 c_vt = .04 # vertical tail volume coefficient 6.26 for sailplane
 c_ht = .7 # horizontal tail volume coefficient 6.27 for sailplane
-
 S_vt = (c_vt * b_w * S_w) / L_vt # vertical tail area
 S_ht = (c_ht * C_w * S_w) / L_ht # horizontal tail area
-
 # define dimensions and project
+theta = np.radians(45) #tail dihedral angle
 
 lamb_t = .6 # tail taper ratio
-b_t = .25 #rudder span
-c_rr = .18 #rudder root chord
-c_rt = c_rr * lamb_t # rudder 
-theta = np.radians(45) # dihedral angle in radians - 60 deg
+c_tr = .18 #tail root chord
+c_tt = c_tr * lamb_t # tail tip chord
+b_t = (2 * S_ht) / ((c_tr + c_tt) * np.cos(theta)) # tail span on dihedral
+
 
 
 """
@@ -61,11 +56,12 @@ Control Surfaces
 """
 
 #rudder sizes
-c_r = .35 * c_vt # rudder chord 25% < x < 50%
+c_rr = .35 * c_tr # rudder chord 25% < x < 50%
 b_r = .9 * b_t # rudder span
 
-c_a = .24 * c_r # aileron root from figure
-b_a = .5 * b # aileron span from figure
+# aileron sizes
+c_a = .3 * c_r # aileron root from figure
+b_a = .25 * b # aileron span from figure
 
 
 """
@@ -83,20 +79,30 @@ R_neg = v_c**2 / (g * np.sqrt(n_min**2 -1))
 """
 Dynamic forces  
 """
-a_f = np.radians(12) # flap extension angle
-A_a = .5 * c_a * b_a # individual aileron area
+a_f = np.radians(15) # flap extension angle
+s_a = c_a * b_a # individual aileron area
+F_a = 2 * q * s_a * (np.sin(a_f))**2
 
-F_a = 2 * q * A_a * (np.sin(a_f))**2
-
-A_r = b_r * .35 * .5 * (c_rr + c_rt)
-F_r = 2 * q * A_r * (np.sin(a_f))**2
+s_r = b_r * .35 * .5 * (c_rr + c_rr) # rudder area
+F_r = 2 * q * s_r * (np.sin(a_f))**2 #force on rudder
 
 if Print:
-    print("Required lift coefficient: ", c_lr)
-    print("stall speed: ", v_s)
-    print("positive turn Radius: ", R_pos)
-    print("negative turn radius: ", R_neg)    
-    print("vertical tail area: ", S_vt)
-    print("Horizontal tail area: ", S_ht)
-    print("Force on aileron: ", F_a)
-    print("Force on rudder: ", F_r)
+    print("Flight Properties")
+    print(f"Required lift coefficient: {c_lr:.4f}")
+    print(f"Stall Speed: {v_s:.2f} m/s")
+    print(f"Minimum Turn Radius: {R_pos:.2f} m ")
+    
+    print("Wing Sizes")
+    print(f"Wing Span: {b} m ")
+    print(f"Chord: {c_r*100} cm")
+    print(f"Predicted wing weight {w_w:.2f} kg")
+    
+    print("Tail Size")
+    print(f"Tail Root Chord: {c_tr*100:.2f} cm")
+    print(f"Tail Tip Chord: {c_tt*100:.2f} cm")
+    print(f"Tail Span: {b_t*100:.2f} cm")
+    print(f"Dihedral Angle: {np.degrees(theta)} Deg")
+    
+    print("Forces")
+    print(f"Force on aileron: {F_a:.2f} N")
+    print(f"Force on rudder: {F_r:.2f} N")
